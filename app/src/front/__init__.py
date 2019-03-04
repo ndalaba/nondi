81f37086f2af3e73aa21bba3624bc2336958c0a8
N'Dalaba Diallo <dmn@dev-hoster.com>
2019-03-04 13:23:29 +0100

    article, video, contact

--- app/src/front/__init__.py
+++ app/src/front/__init__.py
@@ -3,3 +3,12 @@ from flask import Blueprint
 front = Blueprint('front', __name__)
 
 from . import views
+from app.src.entity.Article import Article
+from app.src.entity.Page import Page
+
+
+@front.context_processor
+def inject_value():
+    populars = Article.query.filter_by(published=True).order_by(Article.vue.desc()).paginate(1, 3, False)
+    pages = Page.query.filter_by(published=True).paginate(1, 3, False)
+    return dict(populars=populars.items, pages=pages.items)
--- app/src/front/views.py
+++ app/src/front/views.py
@@ -4,13 +4,13 @@ from flask_mail import Message as Msg
 from app import mail
 from app.src.entity.Article import Article, Category
 from app.src.entity.Message import Message
+from app.src.entity.Page import Page
+from app.src.entity.Person import Person
 from app.src.entity.User import User
 from app.src.entity.Video import Video
 from app.src.repository.Repository import Repository
 from . import front
 from .form import EmailForm
-
-populars = Article.query.filter_by(published=True).order_by(Article.vue.desc()).paginate(1, 3, False)
 
 
 @front.route('/')
@@ -20,8 +20,42 @@ def index():
     blogs = Article.query.filter_by(published=True, category_id=7, top=False).order_by(Article.created_at.desc()).paginate(1, 4, False)
     blog = Article.query.filter_by(published=True, category_id=7, top=True).order_by(Article.created_at.desc()).paginate(1, 1, False)
     articles = Article.query.filter_by(published=True, category_id=6, top=False).order_by(Article.created_at.desc()).paginate(1, 4, False)
-    article = Article.query.filter_by(published=True, category_id=6, top=True).order_by(Article.created_at.desc()).paginate(1, 1, False)
-    return render_template('front/home.html', videos=videos.items, blog=blog.items, blogs=blogs.items, articles=articles.items, article=article.items, populars=populars.items, divers=divers.items)
+    article = Article.query.filter_by(published=True, category_id=6, top=True).order_by(Article.created_at.desc()).paginate(2, 1, False)
+    top = Article.query.filter_by(published=True, category_id=6, top=True).order_by(Article.created_at.desc()).paginate(1, 1, False)
+    return render_template('front/home.html', videos=videos.items, blog=blog.items, blogs=blogs.items, articles=articles.items, article=article.items, divers=divers.items, top=top.items[0])
+
+
+@front.route('/videos/<slug>')
+def videos(slug):
+    page = request.args.get('page', 1, type=int)
+    video = Video.query.filter_by(published=True, slug=slug).first()
+    if video is not None:
+        videos = Video.query.filter(Video.published == True, Video.id != video.id).order_by(Video.created_at.desc()).paginate(page, 6, False)
+        next_url = url_for('front.videos', page=videos.next_num, slug=slug) if videos.has_next else None
+        prev_url = url_for('front.videos', page=videos.prev_num, slug=slug) if videos.has_prev else None
+        return render_template('front/videos.html', video=video, videos=videos.items, next_url=next_url, prev_url=prev_url)
+    else:
+        return redirect(url_for('front.article', category=slug, slug=slug))
+
+
+@front.route('/nondi/<slug>')
+def page(slug):
+    page = Page.query.filter_by(slug=slug).first()
+    if page is not None:
+        persons = Person.query.filter_by(published=True).all()
+        return render_template('front/page.html', page=page, persons=persons)
+    else:
+        return redirect(url_for('front.article', category=slug, slug=slug))
+
+
+@front.route('/collaborateurs/<slug>')
+def persons(slug):
+    person = Person.query.filter_by(published=True, slug=slug).first()
+    if person is not None:
+        persons = Person.query.filter(Person.published == True, Person.id != person.id).all()
+        return render_template('front/person.html', person=person, persons=persons)
+    else:
+        return redirect(url_for('front.article', category=slug, slug=slug))
 
 
 @front.route('/<cat_slug>')
@@ -32,7 +66,7 @@ def articles(cat_slug):
         articles = Article.query.filter_by(published=True,category_id=category.id).order_by(Article.created_at.desc()).paginate(page, Article.POSTS_PER_PAGE, False)
         next_url = url_for('front.articles', page=articles.next_num, cat_slug=cat_slug) if articles.has_next else None
         prev_url = url_for('front.articles', page=articles.prev_num, cat_slug=cat_slug) if articles.has_prev else None
-        return render_template('front/articles.html', article=article, populars=populars.items, articles=articles.items, next_url=next_url, prev_url=prev_url, category=category)
+        return render_template('front/articles.html', article=article, articles=articles.items, next_url=next_url, prev_url=prev_url, category=category)
     else:
         return redirect(url_for('front.article', category=cat_slug, slug=''))
 
@@ -46,30 +80,28 @@ def article(category, slug):
         articles = Article.query.filter(Article.published == True, Article.category_id == article.category_id, Article.id != article.id).order_by(Article.created_at.desc()).paginate(1, 3, False)
     else:
         articles = Article.query.filter(Article.published == True).order_by(Article.created_at.desc()).paginate(1, 10, False)
-    return render_template('front/article.html', article=article, populars=populars.items, articles=articles.items)
+    return render_template('front/article.html', article=article, articles=articles.items)
 
 
-@front.route('/contact', methods=['POST'])
+@front.route('/contact', methods=['POST', 'GET'])
 def contact():
     form = EmailForm()
     if request.method == 'POST':
         if form.is_submitted():
-            user = User.query.filter_by(uid=form.user.data).first()
-            if user is None:
-                return jsonify(type="error", text="Erreur formulaire.")
-            email = Message(user_id=user.id)
+            email = Message(user_id=2)
             email.email_from = form.email.data
             email.folder = "INBOX"
-            email.email_to = user.email
+            email.email_to = "contact@nondi.org"
             email.subject = form.subject.data
             email.message = form.message.data
             email.name = form.name.data
             Repository.save(email)
             
-            msg = Msg(email.subject, sender=(email.name,email.email_from), recipients=[user.email])
+            msg = Msg(email.subject, sender=(email.name,email.email_from), recipients=["contact@nondi.org"])
             msg.body = email.message
             mail.send(msg)
 
             return jsonify(type="success", text="Votre message a été envoyé.")
         return jsonify(type="error", text="Erreur formulaire.")
-    return redirect(url_for('front.index'))
+    else:
+        return render_template('front/contact.html')
